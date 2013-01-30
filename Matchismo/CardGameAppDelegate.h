2ab0e7afb3da6e24e155543596092b62af8c4f78
Ryan Zulkoski <ryan.zulkoski@gmail.com>
2013-01-29 21:54:22 -0600

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Ryan Zulkoski on 1/29/13.
+//  Copyright (c) 2013 RZGamer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Ryan Zulkoski on 1/29/13.
+//  Copyright (c) 2013 RZGamer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
