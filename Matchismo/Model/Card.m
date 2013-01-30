af9c598cd397ca8a149877412ba8d1fd8a7ea492
Ryan Zulkoski <ryan.zulkoski@gmail.com>
2013-01-29 23:45:13 -0600

    Filled out stub classes with code from the lectures. Now ready to begin assignment 1.

--- Matchismo/Model/Card.m
+++ Matchismo/Model/Card.m
@@ -8,6 +8,21 @@
 
 #import "Card.h"
 
+@interface Card()
+
+@end
+
 @implementation Card
+
+- (int)match:(Card *)card
+{
+    int score = 0;
+    
+    if ([card.contents isEqualToString:self.contents]) {
+        score = 1;
+    }
+    
+    return score;
+}
 
 @end
