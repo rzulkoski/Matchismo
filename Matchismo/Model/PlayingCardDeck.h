e18989bb2499210aa16130308899d613bc9deaa7
Ryan Zulkoski <ryan.zulkoski@gmail.com>
2013-01-29 23:18:06 -0600

    Added stub file for PlayingCardDeck.

---
+++ Matchismo/Model/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by Ryan Zulkoski on 1/29/13.
+//  Copyright (c) 2013 RZGamer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlayingCardDeck : NSObject
+
+@end
---
+++ Matchismo/Model/PlayingCardDeck.m
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.m
+//  Matchismo
+//
+//  Created by Ryan Zulkoski on 1/29/13.
+//  Copyright (c) 2013 RZGamer. All rights reserved.
+//
+
+#import "PlayingCardDeck.h"
+
+@implementation PlayingCardDeck
+
+@end
