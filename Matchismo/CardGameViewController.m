35995acca20db7c8771ef261739930a81492d58a
Ryan Zulkoski <ryan.zulkoski@gmail.com>
2013-01-30 12:55:27 -0600

    Added a playing card deck to the CardGameViewController and hooked up an outlet collection to my 12 buttons so that the 12 cards shown will all be random cards drawn from that deck.

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -7,13 +7,32 @@
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
+@property (strong, nonatomic) Deck *deck;
 @end
 
 @implementation CardGameViewController
+
+- (Deck *)deck
+{
+    if (!_deck) _deck = [[PlayingCardDeck alloc] init];
+    return _deck;
+}
+
+- (void)setCardButtons:(NSArray *)cardButtons
+{
+    _cardButtons = cardButtons;
+    for (UIButton *cardButton in cardButtons) {
+        Card *card = [self.deck drawRandomCard];
+        NSLog(@"Random card = %@", card.contents);
+        [cardButton setTitle:card.contents forState:UIControlStateSelected];
+    }
+}
 
 - (void)setFlipCount:(int)flipCount
 {
